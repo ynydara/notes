3dbc5ff192315d11cdca05825fd547be645f707a
W0718860 <alyssa.major@selu.edu>
2022-08-31 15:04:59 -0500

    notes from 8/31/2022

---
+++ Bottle.java
@@ -0,0 +1,24 @@
+
+public class Bottle {
+
+        //attributes
+        int volume;
+        String material;
+
+        //constructors
+        Bottle() {
+            //default constructor
+            this(500, "plastic");
+
+        }
+        Bottle( int volume, String material){
+
+            if (volume > 0)
+                this.volume = volume;
+            else {
+               this. volume = 500;
+            }
+            this.material = material;
+        }
+    }
+
---
+++ code_from_8312022.java
@@ -0,0 +1,10 @@
+public class code_from_8312022 {
+    public static void main(String[] args){
+        Bottle b1 = new Bottle();//water bottle
+        Bottle b2 = new Bottle(1000, "steel"); //yeti
+        Bottle b3 = new Bottle(-1000, "dark matter");//thanos bottle
+
+        System.out.print("Water bottle volume" + b1.volume);
+        System.out.print("Water bottle material: " + b1.material);
+    }
+}
