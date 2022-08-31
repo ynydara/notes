dc1459f34b714b3df1406274c9a15a374fbef592
W0718860 <alyssa.major@selu.edu>
2022-08-31 14:28:34 -0500

    notes from 8/31/2022

--- notes1.java
+++ notes1.java
@@ -1,6 +1,59 @@
 public class notes1 {
-    public static void main(String[] args) {
-     //test
+//----------------------8/31/2022-----------------------------------------------------------------
+    //constructors are used to initilize object.
+    //main{
+    //    Marker o = new [Marker()] - constructor;
+    //if no constructor is specified, java adds a default empty one.
+    //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    //main {
+    //  Bottle b = new Bottle();
+    //      b.volume = 500; | b.material = "plastic"; V
+    //      wouldn't be needed if have the Bottle() constructors because all bottles will have these attributes.
+    //if its a different one than the default.
+    //  Bottle c = new Bottle ()
+    //Sys.out.print(c.volume);//console:500
+    //
+//}
+    // --> constructor can be overloaded.
+    //   class Bottle{
+    //   characteristics://list of attributes;
+    //          int volume;
+    //          string material;
+    //   constructors need to be defined
+    //      Bottle(){
+    //          volume = 500;
+    //          material = "plastic";
+    //}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// main{
+    //Bottle b = new Bottle (1000, "steel"); java will use the one with the signature.
+    //sys.out....(b.volume); |console: 1000
 
-    }
+//Bottle c = new Bottle (); java will use the default values
+    //sys.out....(c.volume); |console: 500
+//}
+    //class Bottle{
+    //int volume;
+    //String materal;
+    //constructors
+    //Bottle(){
+    //volume = 500;
+    // material = "plastic";
+    //you can have the same constructors only if they have different signatures/
+//this is example of overloading.
+//}
+    //Bottle(int vol, String mat){
+        //volume = vol;
+    //material = mat;
+
+
+    //}
+
+
+    //}
+//}
+
+
+
 }
